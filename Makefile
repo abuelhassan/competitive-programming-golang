542da7fe71b26cb05cba53fe54b1cf8c1bb4d91b
mohamed.amr@wizeline.com <mohamed.amr@wizeline.com>
2020-06-12 15:15:29 +0200

    add support for Spoj

--- Makefile
+++ Makefile
@@ -5,8 +5,11 @@ PROBS = a b c d e f
 $(PROBS):
 	@make g/contest/$@
 
+m/%: templates/mainstream/template.go
+	@[ -f $*/main.go ] || (mkdir -p $*; touch $*/main.go; cp $< $*/main.go)
+
 g/%: templates/google-competitions/template.go
 	@[ -f $*/main.go ] || (mkdir -p $*; touch $*/main.go; cp $< $*/main.go)
 
 g-contest: $(PROBS)
-	@echo "Google contest created! Good luck señor."
+	@echo "Google contest created!"
---
+++ templates/mainstream/template.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	//io := newFastIO()
+	//defer io.Flush()
+}
+
+type fastIO struct {
+	r *bufio.Reader
+	w *bufio.Writer
+}
+
+func newFastIO() fastIO {
+	return fastIO{
+		r: bufio.NewReader(os.Stdin),
+		w: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *fastIO) Read(args ...interface{}) {
+	_, _ = fmt.Fscan(io.r, args...)
+}
+
+func (io *fastIO) Write(format string, args ...interface{}) {
+	_, _ = fmt.Fprintf(io.w, format, args...)
+}
+
+func (io *fastIO) Flush() {
+	_ = io.w.Flush()
+}
