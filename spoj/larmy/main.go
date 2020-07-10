5bf021330bde1bacb52f1566d48e2bc72114e57f
mohamed.amr@wizeline.com <mohamed.amr@wizeline.com>
2020-07-10 10:52:22 +0200

    spoj: larmy - Lannister Army - use arrays instead of slices (TLE)

--- spoj/larmy/main.go
+++ spoj/larmy/main.go
@@ -8,28 +8,37 @@ import (
 	"os"
 )
 
+const maxN = 5001
+
+var (
+	row  = [maxN]int{}
+	memo = [2][maxN]int{}
+	cost = [maxN][maxN]int{}
+)
+
 // DP, Divide and Conquer Optimization
-func rowDivideAndConquer(l, r, optL, optR int, dpBefore, dpCur []int, cost [][]int) {
+func rowDivideAndConquer(rowIdx, l, r, optL, optR int) {
 	if l > r {
 		return
 	}
 
 	var (
-		mid = (l + r) >> 1
-		ans = len(cost) * len(cost)
-		opt = -1
+		mid       = (l + r) >> 1
+		ans       = maxN * maxN
+		opt       = -1
+		prvRowIdx = (rowIdx + 1) % 2
 	)
-	for k := optL; k <= mid && k <= optR && k < len(cost)-1; k++ {
-		t := dpBefore[k] + cost[mid][k+1]
+	for k := optL; k <= mid && k <= optR; k++ {
+		t := memo[prvRowIdx][k] + cost[mid][k+1]
 		if t < ans {
 			ans = t
 			opt = k
 		}
 	}
 
-	dpCur[mid] = ans
-	rowDivideAndConquer(l, mid-1, optL, opt, dpBefore, dpCur, cost)
-	rowDivideAndConquer(mid+1, r, opt, optR, dpBefore, dpCur, cost)
+	memo[rowIdx][mid] = ans
+	rowDivideAndConquer(rowIdx, l, mid-1, optL, opt)
+	rowDivideAndConquer(rowIdx, mid+1, r, opt, optR)
 }
 
 func main() {
@@ -39,14 +48,11 @@ func main() {
 
 	var n, m int
 	_, _ = fmt.Fscan(r, &n, &m)
-	row := make([]int, n)
 	for i := 0; i < n; i++ {
 		_, _ = fmt.Fscan(r, &row[i])
 	}
 
-	cost := make([][]int, n)
 	for i := n - 1; i >= 0; i-- {
-		cost[i] = make([]int, n)
 		for j := i - 1; j >= 0; j-- {
 			cost[i][j] += cost[i][j+1]
 			if row[i] < row[j] {
@@ -60,15 +66,11 @@ func main() {
 		}
 	}
 
-	memo := make([][]int, 2)
-	for i := 0; i < len(memo); i++ {
-		memo[i] = make([]int, n)
-	}
 	for i := 0; i < n; i++ {
 		memo[0][i] = cost[i][0]
 	}
 	for i := 1; i < m; i++ {
-		rowDivideAndConquer(0, n-1, 0, n-1, memo[(i-1)%2], memo[i%2], cost)
+		rowDivideAndConquer(i%2, 0, n-1, 0, n-1)
 	}
 	_, _ = fmt.Fprintf(w, "%d\n", memo[(m-1)%2][n-1])
 }
