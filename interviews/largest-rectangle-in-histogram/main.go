a5f3eba4c77dcf56096f89ce1c85c75d2c50a0d8
amr <abuelhassan93@gmail.com>
2021-07-17 08:02:24 +0200

    interviews: largest rectangle in histogram

---
+++ interviews/largest-rectangle-in-histogram/main.go
@@ -0,0 +1,68 @@
+// Given an array of integers heights representing the histogram's bar height where
+// the width of each bar is 1, return the area of the largest rectangle in the histogram.
+package main
+
+import "fmt"
+
+type stack []int
+
+// Using a stack with ascending values. O(N) solution.
+func largestRectangleArea(heights []int) int {
+	n, ans, st := len(heights), 0, stack{-1}
+	for i := 0; i < n; i++ {
+		for st.top() != -1 && heights[st.top()] >= heights[i] {
+			cur := st.pop()
+			area := (i - st.top() - 1) * heights[cur] // width * height
+			if area > ans {
+				ans = area
+			}
+		}
+		st.push(i)
+	}
+	for st.top() != -1 {
+		cur := st.pop()
+		area := (n - st.top() - 1) * heights[cur] // width * height
+		if area > ans {
+			ans = area
+		}
+	}
+	return ans
+}
+
+func (st *stack) push(a int) {
+	*st = append(*st, a)
+}
+func (st *stack) pop() int {
+	elem := (*st)[len(*st)-1]
+	*st = (*st)[:len(*st)-1]
+	return elem
+}
+func (st *stack) top() int {
+	return (*st)[len(*st)-1]
+}
+
+func main() {
+	tt := []struct {
+		in  []int
+		out int
+	}{
+		{
+			in:  []int{2, 1, 5, 6, 2, 3},
+			out: 10,
+		},
+		{
+			in:  []int{2, 4, 5, 5, 4, 4, 3},
+			out: 20,
+		},
+		{
+			in:  []int{2, 4, 5, 5, 3, 3, 3, 1},
+			out: 18,
+		},
+	}
+	for _, t := range tt {
+		if out := largestRectangleArea(t.in); out != t.out {
+			panic(fmt.Sprintf("\ninput: %v\noutput: %v\nexpected: %v\n", t.in, out, t.out))
+		}
+	}
+	fmt.Println("Todo bien!")
+}
